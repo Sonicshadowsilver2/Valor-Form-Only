52e499a13a99af931ae7e160e1b9006fbab5a0a3
Sonicshadowsilver2 <danielctip2018@gmail.com>
2025-09-10 03:41:53 -0500

    Update ValorFormOnly.lua

--- ValorFormOnly.lua
+++ ValorFormOnly.lua
@@ -13,8 +13,7 @@ function GetVersion() --Define anchor addresses
 			Now  = 0x0716DF8
 			Save = 0x09A92F0
 			UCM = 0x2A71998
-			Obj0Pointer = 0x2A24A70
-			Obj0 = ReadLong(Obj0Pointer)
+			Obj0 = 0x2A24A70
 			canExecute = true
 		elseif ReadString(0x09A9330,4) == 'KH2J' then --EGS v1.0.0.10
 			GameVersion = 3
@@ -22,8 +21,7 @@ function GetVersion() --Define anchor addresses
 			Now  = 0x0716DF8
 			Save = 0x09A9330
 			UCM = 0x2A719D8
-			Obj0Pointer = 0x2A24AB0
-			Obj0 = ReadLong(Obj0Pointer)
+			Obj0 = 0x2A24AB0
 			canExecute = true
 		elseif ReadString(0x09A9830,4) == 'KH2J' then --Steam Global v1.0.0.1
 			GameVersion = 4
@@ -70,10 +68,11 @@ function _OnFrame()
 			else
 				WriteShort(UCM+0x009C, 0x0055) --Roxas -> Valor Form
 			end
-			if ReadByte(Now+0x08) ~= 0x89 then --Axel II
-				WriteShort(Save+0x34EC, 0x01E0) --Oathkeeper -> Star Seeker
-			else
-				WriteShort(Save+0x34EC, 0x002A) --Oathkeeper -> Oathkeeper
+			if ReadByte(Now+0x08) == 0x89 then --Valor Form Keyblade at Axel II
+				WriteShort(Save+0x32F4, 0x002A)
+			end
+			if ReadByte(Now+0x08) == 0x87 then --Valor Form Keyblade after Axel II
+				WriteShort(Save+0x32F4, 0x01E0)
 			end
 		end
 		if ReadByte(Now+0x00) == 0x0A then --Pride Lands
@@ -84,14 +83,20 @@ function _OnFrame()
 			end
 		end
 		if ReadByte(UCM+0x00) == 0x54 then
-			WriteShort(UCM+0x0000, 0x0055) --Sora (Normal)
-			WriteShort(UCM+0x00D0, 0x0055) --Roxas (Dual-Wielded)
-			WriteShort(UCM+0x0104, 0x0055) --Sora (KH1 Costume)
-			WriteShort(UCM+0x0444, 0x03E6) --Sora (Halloween Town)
-			WriteShort(UCM+0x0478, 0x0956) --Sora (Christmas Town)
-			WriteShort(UCM+0x0680, 0x0669) --Sora (Space Paranoids)
-			WriteShort(UCM+0x06B4, 0x066A) --Sora (Timeless River)
-			WriteString(Obj0+0x154D0,'F_TT010_SORA.mset\0') --Skateboard (Roxas)
+			WriteShort(UCM+0x0000, 0x0055) --Sora (Normal) -> Valor Form
+			WriteShort(UCM+0x00D0, 0x0055) --Roxas (Dual-Wielded) -> Valor Form
+			WriteShort(UCM+0x0104, 0x0055) --Sora (KH1 Costume) -> Valor Form
+			WriteShort(UCM+0x0444, 0x03E6) --Sora (Halloween Town) -> Valor Form (Halloween Town)
+			WriteShort(UCM+0x0478, 0x0956) --Sora (Christmas Town) -> Valor Form (Christmas Town)
+			WriteShort(UCM+0x0680, 0x0669) --Sora (Space Paranoids) -> Valor Form (Space Paranoids)
+			WriteShort(UCM+0x06B4, 0x066A) --Sora (Timeless River) -> Valor Form (Timeless River)
+			WriteString(Obj0+0x0C870,'P_EX100_BTLF\0') --Sora (Blustery Rescue) Model -> Valor Form Model
+			WriteString(Obj0+0x0C890,'P_EX100_BTLF.mset\0') --Sora (Blustery Rescue) MSET -> Valor Form MSET
+			WriteString(Obj0+0x0C8C0,'P_EX100_BTLF\0') --Sora (Hunny Slider) Model -> Valor Form Model
+			WriteString(Obj0+0x0C8E0,'P_EX100_BTLF.mset\0') --Sora (Hunny Slider) MSET -> Valor Form MSET
+			WriteString(Obj0+0x12690,'P_EX100_BTLF\0') --Sora (On Carpet) Model -> Valor Form Model
+			WriteString(Obj0+0x154D0,'F_TT010_SORA.mset\0') --Skateboard (Roxas) MSET -> Skateboard (Sora) MSET
+			WriteString(Obj0+0x22E90,'P_EX100_BTLF\0') --Sora (Light Cycle) Model -> Valor Form Model
 		end
 	end
 end
